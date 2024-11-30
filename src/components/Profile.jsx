4f40d475806f31dc758f305455da4387f1306cd7
Himanshu Tiwari <htiwari578@gmail.com>
2024-11-30 13:47:40 +0530

    user-profile

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 
 const Profile = () => {
   return (
-    <div>
-        <h2>User Profile</h2>
+    <div className="bg-white p-6 rounded-lg shadow-lg ">
+        <h2 className="text-xl font-bold mb-4">User Profile</h2>
+        <p>Name:Himanshu</p>
+        <p>Email:Himansh@.com</p>
+        <p>Account created:Nov 2024</p>
     </div>
   )
 }
